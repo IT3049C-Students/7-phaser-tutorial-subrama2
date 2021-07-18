2e0350f2f9660d4271bae0472d56115bacbe9ffe
Annamalai Subramanian <subrama2@mail.uc.edu>
2021-07-18 00:28:05 -0400

    added images to pre loaded function

--- Scene1.js
+++ Scene1.js
@@ -4,8 +4,11 @@ class Scene1 extends Phaser.Scene {
     }
     preload(){
         this.load.image("background", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\background.png");
+        this.load.image("ship", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship.png");
+        this.load.image("ship2","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship2.png");
+        this.load.image("ship3","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship3.png");
     }
-    
+
     create() {
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
