af05fb578a1936dfa83061ff884d6cdf2f91a17f
Annamalai Subramanian <subrama2@mail.uc.edu>
2021-07-18 01:08:26 -0400

    spritesheet images added with frame values

--- Scene1.js
+++ Scene1.js
@@ -4,9 +4,29 @@ class Scene1 extends Phaser.Scene {
     }
     preload(){
         this.load.image("background", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\background.png");
-        this.load.image("ship", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship.png");
-        this.load.image("ship2","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship2.png");
-        this.load.image("ship3","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship3.png");
+       
+        this.load.spritesheet("ship", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\spritesheets\ship.png",{
+            frameWidth: 16,
+            frameHeight: 16
+
+        });
+        //this.load.image("ship2","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship2.png");
+        //this.load.image("ship3","C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\assets\ship3.png");
+        this.load.spritesheet("ship2", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\spritesheets\ship2.png",{
+            frameWidth: 32,
+            frameHeight: 16
+
+        });
+        this.load.spritesheet("ship3", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\spritesheets\ship3.png",{
+            frameWidth: 32,
+            frameHeight: 32
+
+        });
+        this.load.spritesheet("explosion", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\spritesheets\explosion.png",{
+            frameWidth: 16,
+            frameHeight: 16
+
+        });
     }
 
     create() {
