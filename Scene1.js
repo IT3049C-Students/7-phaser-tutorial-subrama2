5f1cc07762a4b5bbbc16585288a32b3e34876694
Annamalai Subramanian <subrama2@mail.uc.edu>
2021-07-18 02:47:44 -0400

    added player image for preloaded function

--- Scene1.js
+++ Scene1.js
@@ -32,11 +32,88 @@ class Scene1 extends Phaser.Scene {
             frameHeight: 16
 
         });
+        this.load.spritesheet("player", "C:\Annamalai\2021 Summer semester\Web Game Development\7-phaser-tutorial-subrama2\spritesheets\player.png",{
+            frameWidth: 16,
+            frameHeight: 24
+
+        });
     }
 
     create() {
-        this.add.text(20, 20, "Loading game...");
+
+        this.add.text(20,20, "Loading game...");
         this.scene.start("playGame");
+
+        this.anims.create({
+            key: "ship1_anim",
+            frames: this.anims.generateFrameNumbers("ship"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship2_anim",
+            frames: this.anims.generateFrameNumbers("ship2"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "ship3_anim",
+            frames: this.anims.generateFrameNumbers("ship3"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion"),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
+
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        this.input.on('gameobjectdown', this.destroyShip, this);
+        this.add.text(20, 20, "Playing game", {
+            font: "25px Arial",
+            fill: "yellow"
+        });
+        this.anims.create({
+
+            key: "red",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+        
+        this.anims.create({
+
+            key: "gray",
+            frames: this.anims.generateFrameNumbers("power-up", {
+                start: 2,
+                end: 3
+            }),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+
+            key: "thrust",
+            frames: this.anims.generateFrameNumbers("player"),
+            frameRate: 20,
+            repeat: -1
+        });
     }
     
 }
